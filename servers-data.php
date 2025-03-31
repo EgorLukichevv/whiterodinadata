1e4a0c0203898263cf4a558ed64df85a9e917378
EgorLukichevv <lukichev561z@gmail.com>
2025-03-31 10:48:48 +0300

    Update servers-data.php

--- servers-data.php
+++ servers-data.php
@@ -1,15 +1,1 @@
-			{
-				"rodina": 
-				[
-					{
-						"number": 1,
-						"name": "Вайтовский округ",
-						"ip": "94.198.51.12",
-						"port": 2059,
-						"online": "20",
-						"maxplayers": "300",
-						"password": false,
-						"icon": "https:\/\/arizona-recovery.react.domains\/desktop\/resources\/icons\/rodina\/1.png"
-					}
-				]
-			}
+{"rodina":[{"number":1,"name":"Белый округ","ip":"94.198.51.12","port":2059,"online":4,"maxplayers":300,"password":false,"vk":"","tg":"","inst":"","icon":"https://arizona-recovery.react.domains/desktop/resources/icons/arizona/1.png"}]}
