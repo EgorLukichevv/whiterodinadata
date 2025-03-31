ebe0884cdeb085f150f524516eb884e44c403b80
EgorLukichevv <lukichev561z@gmail.com>
2025-03-31 09:38:18 +0300

    Update servers-data.php

--- servers-data.php
+++ servers-data.php
@@ -6,8 +6,8 @@
 						"name": "Вайтовский округ",
 						"ip": "94.198.51.12",
 						"port": 2059,
-						"online": 22,
-						"maxplayers": 300,
+						"online": Доступно только,
+						"maxplayers": админам!,
 						"password": false,
 						"icon": "https:\/\/arizona-recovery.react.domains\/desktop\/resources\/icons\/rodina\/1.png"
 					}
